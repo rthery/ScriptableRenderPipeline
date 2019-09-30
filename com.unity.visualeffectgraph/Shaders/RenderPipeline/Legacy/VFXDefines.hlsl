c10a6c7df1fd7192e33d8d295625e6736a52c18d
Paul Demeulenaere <pauld@unity3d.com>
2019-09-30 17:15:18 +0200

    Avoid breaking legacy compilation

--- com.unity.visualeffectgraph/Shaders/RenderPipeline/Legacy/VFXDefines.hlsl
+++ com.unity.visualeffectgraph/Shaders/RenderPipeline/Legacy/VFXDefines.hlsl
@@ -1,3 +1,13 @@
 #include "HLSLSupport.cginc"
 
 #define UNITY_VERTEX_OUTPUT_STEREO // So that templates compile
+
+//Additionnal empty wrapper (equivalent to expected function in com.unity.render-pipelines.core/ShaderLibrary/SpaceTransforms.hlsl)
+float3 GetAbsolutePositionWS(float3 positionRWS)
+{
+    return positionRWS;
+}
+float3 GetCameraRelativePositionWS(float3 positionWS)
+{
+    return positionWS;
+}
